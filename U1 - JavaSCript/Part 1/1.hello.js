ef7c088a3e772d798913a8e570de45425429e083
Arturo Bernal <arturober@gmail.com>
2025-09-15 12:51:18 +0200

    ejemplos

--- U1 - JavaSCript/Part 1/1.hello.js
+++ U1 - JavaSCript/Part 1/1.hello.js
@@ -1,1 +1,33 @@
-console.log("Hello world!");
+'use strict';
+
+/**
+ * Devuelve la suma de n1 + n2
+ * @param {number} n1 
+ * @param {number} n2 
+ * @returns {number} Suma de n1 + n2 
+ */
+function suma(n1, n2) {
+    return n1 + n2;
+}
+
+function suma2(n1, n2) {
+    return n1 + n2;
+}
+
+let div = document.getElementById("div");
+for(let i = 1; i <= 10; i++) {
+    let p = document.createElement("p");
+    p.textContent = `Párrafo ${i}`;
+    p.addEventListener('click', () => console.log(`Párrafo ${i}`));
+    div.append(p);
+}
+
+// num = 45;
+// console.log(num);
+
+function f(nombre) {
+    console.log(`Hola ${nombre ?? 'Anónimo'}`);
+}
+
+f();
+f("Pepe");
