3a784899279eb9f382fbf25beabc98eeff07962d
Iván Fuster <ifussim@outlook.com>
2025-11-17 17:48:55 +0100

    reestructuracion de carpetas

--- Ejercicios/Parte-4/Ejercicio/src/constants.js
+++
@@ -1,9 +0,0 @@
-export const SERVER = "http://localhost:3000";
-
-export const form = document.getElementById("property-form");
-export const imgPreview = document.getElementById("image-preview");
-export const listings = document.getElementById("property-listings");
-export const template = document.getElementById("property-card-template");
-
-export const selectProvince = document.getElementById("province");
-export const selectTown = document.getElementById("town");
